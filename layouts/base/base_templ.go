66e215b52f8da75895768908d4dacb6d36489c56
KennyZ69 <artur.paulik06@gmail.com>
2024-06-30 22:32:02 +0200

    footer added

--- layouts/base/base_templ.go
+++ layouts/base/base_templ.go
@@ -77,6 +77,10 @@ func Base(r *http.Request, data bool, username string) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
+		templ_7745c5c3_Err = Footer().Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
 		templ_7745c5c3_Err = templ_7745c5c3_Var1.Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
